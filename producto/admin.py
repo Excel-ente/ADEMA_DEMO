9c1c010558dd8fb7acd753d87f1bf8b9fc97b251
Excel-ente <turkienich@gmail.com>
2023-07-28 23:04:49 -0300

    Cambios

--- producto/admin.py
+++ producto/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from producto.models import Producto, Categoria
 
 
 @admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaAdmin(ImportExportModelAdmin):
     list_display = ('nombre',)
 
 @admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
+class ProductoAdmin(ImportExportModelAdmin):
     list_display = ('categoria','nombre','descripcion','costo','precio_venta','en_stock')
     list_filter = ('categoria','nombre','descripcion')
     search_fields = ('nombre',)
